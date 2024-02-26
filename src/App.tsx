190b2acf250ed15afe757d4b27dd77f349e22245
Glendson <gztomazetto@gmail.com>
2024-02-26 16:48:08 +0000

    feat: add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,39 @@ import {
   TableRow,
 } from "./components/ui/table";
 import { Pagination } from "./components/pagination";
+import { useQuery } from "@tanstack/react-query";
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/tags?_page=1&_per_page=10')
+      const data = response.json();
+
+      return data
+    },
+  })
+
+  if(isLoading) return null
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -50,17 +81,17 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">React</span>
-                      <span className="text-sm text-zinc-500">React</span>
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-sm text-zinc-500">{tag.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-500">13 videos</TableCell>
+                  <TableCell className="text-zinc-500">{tag.amountOfVideos} video(s)</TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
                       <MoreHorizontal className="size-4" />
@@ -72,7 +103,9 @@ export function App() {
           </TableBody>
         </Table>
 
-        <Pagination />
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={1}/>}
+
+        
       </main>
     </div>
   );
